ae01fb73904c160694d83126c17ff68d72f97ed7
Lautaro Bachmann <laurigbach@gmail.com>
2022-04-05 10:01:18 -0300

    Started adding tags to notes

--- anki_deck.py
+++ anki_deck.py
@@ -31,11 +31,12 @@ h2 {
         deck_num = random.randrange(1 << 30, 1 << 31)
         self.deck_name = "::".join([pkg_name] + headings)
         self.deck = genanki.Deck(deck_num, self.deck_name)
-        self.tags = " ".join([heading.replace(' ', '_') for heading in headings])
+        self.tags = [heading.replace(' ', '_') for heading in headings]
 
     def add_note(self, title, content):
         note = genanki.Note(
                 model=self.model,
-                fields=[title, content, self.tags]
+                fields=[title, content, " ".join(self.tags)],
+                tags=[self.deck_name.replace(' ', '_')]
             )
         self.deck.add_note(note)
